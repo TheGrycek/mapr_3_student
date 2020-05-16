a2631afb6fceaa508196a71ce8257e6956a4e310
Mateusz Grycmacher <51837956+TheGrycek@users.noreply.github.com>
2020-05-17 01:17:03 +0200

    Zadanie bfs

--- src/bfs_backtrace.py
+++ src/bfs_backtrace.py
@@ -6,24 +6,54 @@ from grid_map import GridMap
 class BFS(GridMap):
     def __init__(self):
         super(BFS, self).__init__()
-        ###  IF YOU NEED SOME ADDITIONAL FILEDS IN BFS OBJECT YOU CAN INITIALIZED TEHM HERE
+        self.prev = [[(0, 0) for i in range(self.map.info.height)] for j in range(self.map.info.width)]
 
     def search(self):
-        ### YOUR CODE GOES BELOW
-        #
-        #
-        # IMPLEMENT BREADTH FIRST SEARCH WITH BACKTRACE:
-        # * save your search in self.map.data
-        # * use self.publish_visited() to publish the map every time you visited a new cell
-        # * let 100 represent walls, 50 visited cells (useful for visualization)
-        # * save the path to the goal fund by the algorithm to list of tuples: [(x_n, y_n), ..., (x_2, y_2), (x_1, y_1)]
-        # * use self.publish_path(path) to publish the path at the very end
-        # * start point is in self.start
-        # * end point is in self.end
-        #
-        #
-        ### YOUR CODE GOES ABOVE
-        pass  # DELETE THIS LINE IF THE CODE INSERTED
+       bord = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+        vis = []
+        queue = [self.start]
+
+        while (len(queue) > 0):
+            vis_b = False
+
+            while (vis_b == False):
+
+                if(queue[0] not in vis):
+                    vis_b = True
+                    c_node = queue[0]
+                else:
+                    queue.pop(0)
+
+            vis.append(c_node)
+            r = c_node[0]
+            c = c_node[1]
+            self.map.data[c + r * self.map.info.width] = 50  # ustawienie węzła jako odwiedzony
+            if (c_node == self.end):
+                break
+
+            for b in bord:
+                if self.map.data[c + b[0] + (r + b[1]) * self.map.info.width] < 50:
+                    queue.append((r + b[1], c + b[0]))
+                    self.prev[r + b[1]][c + b[0]] = c_node
+
+            queue.pop(0)
+            self.publish_visited()
+
+        start_r = False
+        path = []
+        c_node = self.end
+        path.append(c_node)
+
+        while (start_r == False):
+            r = c_node[0]
+            c = c_node[1]
+            c_node = self.prev[r][c]
+            path.append(c_node)
+            if (c_node == self.start):
+                start_r = True
+
+        self.publish_path(path)
+        self.publish_visited()
 
 
 if __name__ == '__main__':
